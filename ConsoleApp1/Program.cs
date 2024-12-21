877bb9029bebce7bb48c397cb09f71049a83e7e0
jj <twojemail@example.com>
2024-12-21 20:56:40 +0100

    Add Concert class implementing IConcert interface

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-public interface ConcertInterface
+public interface IConcert
 {
     string Name { get; }
     DateTime Date { get; }
     string Location { get; }
     int AvailableSeats { get; }
     void BookSeat();
+}
+
+public class Concert : IConcert
+{
+    public string Name { get; private set; }
+    public DateTime Date { get; private set; }
+    public string Location { get; private set; }
+    public int AvailableSeats { get; private set; }
+
+    public Concert(string name, DateTime date, string location, int availableSeats)
+    {
+        Name = name;
+        Date = date;
+        Location = location;
+        AvailableSeats = availableSeats;
+    }
+
+    public virtual void BookSeat()
+    {
+        if (AvailableSeats > 0)
+        {
+            AvailableSeats--;
+            Console.WriteLine($"Miejsce wykupione przez {Name} na {Date.ToShortDateString()}.");
+        }
+        else
+        {
+            Console.WriteLine("Brak wolnych miejsc.");
+        }
+    }
 }
